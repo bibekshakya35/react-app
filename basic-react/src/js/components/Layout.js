7651d5027a0dfd935c4f395831d4e3b9845acad5
bibek shakya <bibekshakya35@gmail.com>
2017-07-25 09:33:41 +0545

    Working on router

---
+++ basic-react/src/js/components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+export default class Layout extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            title:"Welcome"
+        };
+    }
+    changeTitle(title){
+        this.setState({title});
+    }
+    render(){
+        setTimeout(()=>{
+            this.setState({title:"Welcome Bibek"});
+        },3000);
+        return (
+            <div>
+            <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+            <Footer/>
+            </div>
+        );
+    };
+}
---
+++ react-router/src/js/pages/Archived.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default class Archived extends React.Component{
+    render(){
+        return (
+           <h1>Archived</h1>
+        );
+    };
+}
